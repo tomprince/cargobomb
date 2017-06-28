4044974d6536e3d47c67b7689ab115f2e1ca2773
Tom Prince <tom.prince@twistedmatrix.com>
2017-06-28 10:03:43 -0600

    Use transactions.

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -35,30 +35,32 @@ impl Model for DbStore {
         use db::schema::*;
         let conn = self.conn.lock().expect("Poisoined lock");
 
-        let ex: queries::Experiment = experiments::table
-            .filter(experiments::name.eq(ex_name))
-            .get_result(&*conn)?;
-        let tcs = toolchains::table
-            .inner_join(experiment_toolchains::table)
-            .select(toolchains::description)
-            .filter(experiment_toolchains::experiment_id.eq(ex.id))
-            .load(&*conn)?
-            .into_iter()
-            .map(|desc| serde_json::from_value(desc).map_err(From::from))
-            .collect::<Result<_>>()?;
-        let crates = crates::table
-            .inner_join(experiment_crates::table)
-            .select(crates::description)
-            .filter(experiment_crates::experiment_id.eq(ex.id))
-            .load(&*conn)?
-            .into_iter()
-            .map(|desc| serde_json::from_value(desc).map_err(From::from))
-            .collect::<Result<_>>()?;
-        Ok(Experiment {
-            name: ex.name,
-            mode: ex.mode.parse()?,
-            toolchains: tcs,
-            crates: crates,
+        conn.transaction(|| {
+            let ex: queries::Experiment = experiments::table
+                .filter(experiments::name.eq(ex_name))
+                .get_result(&*conn)?;
+            let tcs = toolchains::table
+                .inner_join(experiment_toolchains::table)
+                .select(toolchains::description)
+                .filter(experiment_toolchains::experiment_id.eq(ex.id))
+                .load(&*conn)?
+                .into_iter()
+                .map(|desc| serde_json::from_value(desc).map_err(From::from))
+                .collect::<Result<_>>()?;
+            let crates = crates::table
+                .inner_join(experiment_crates::table)
+                .select(crates::description)
+                .filter(experiment_crates::experiment_id.eq(ex.id))
+                .load(&*conn)?
+                .into_iter()
+                .map(|desc| serde_json::from_value(desc).map_err(From::from))
+                .collect::<Result<_>>()?;
+            Ok(Experiment {
+                name: ex.name,
+                mode: ex.mode.parse()?,
+                toolchains: tcs,
+                crates: crates,
+            })
         })
     }
     fn create_experiment(
@@ -76,86 +78,88 @@ impl Model for DbStore {
 
         use db::schema::*;
         let conn = self.conn.lock().expect("Poisoined lock");
+        conn.transaction(|| {
+            let experiment_id = diesel::insert(&queries::ExperimentInsert {
+                name: ex_name.to_string(),
+                mode: mode.to_str().to_string(),
+            }).into(experiments::table)
+                .returning(experiments::id)
+                .get_result::<i32>(&*conn)?;
+            let crates = crates
+                .into_iter()
+                .map(|c| {
+                    queries::Crate { description: serde_json::to_value(c).unwrap() }
+                })
+                .collect::<Vec<_>>();
+            diesel::insert(&crates.on_conflict_do_nothing())
+                .into(crates::table)
+                .execute(&*conn)?;
+            let crate_ids = crates::table
+                .filter(crates::description.eq_any(
+                    crates.into_iter().map(|c| c.description),
+                ))
+                .select(crates::id)
+                .load::<i32>(&*conn)?;
+            diesel::insert(&crate_ids
+                .into_iter()
+                .map(|crate_id| {
+                    queries::ExperimentCrate {
+                        experiment_id,
+                        crate_id,
+                    }
+                })
+                .collect::<Vec<_>>()).into(experiment_crates::table)
+                .execute(&*conn)?;
+            let tcs = tcs.into_iter()
+                .map(|tc| {
+                    queries::Toolchain { description: serde_json::to_value(tc).unwrap() }
+                })
+                .collect::<Vec<_>>();
+            diesel::insert(&tcs.on_conflict_do_nothing())
+                .into(toolchains::table)
+                .execute(&*conn)?;
+            let toolchain_ids = toolchains::table
+                .filter(toolchains::description.eq_any(tcs.into_iter().map(
+                    |tc| tc.description,
+                )))
+                .select(toolchains::id)
+                .load::<i32>(&*conn)?;
+            diesel::insert(&toolchain_ids
+                .into_iter()
+                .map(|toolchain_id| {
+                    queries::ExperimentToolchain {
+                        experiment_id,
+                        toolchain_id,
+                    }
+                })
+                .collect::<Vec<_>>()).into(experiment_toolchains::table)
+                .execute(&*conn)?;
 
-        let experiment_id = diesel::insert(&queries::ExperimentInsert {
-            name: ex_name.to_string(),
-            mode: mode.to_str().to_string(),
-        }).into(experiments::table)
-            .returning(experiments::id)
-            .get_result::<i32>(&*conn)?;
-        let crates = crates
-            .into_iter()
-            .map(|c| {
-                queries::Crate { description: serde_json::to_value(c).unwrap() }
-            })
-            .collect::<Vec<_>>();
-        diesel::insert(&crates.on_conflict_do_nothing())
-            .into(crates::table)
-            .execute(&*conn)?;
-        let crate_ids = crates::table
-            .filter(crates::description.eq_any(
-                crates.into_iter().map(|c| c.description),
-            ))
-            .select(crates::id)
-            .load::<i32>(&*conn)?;
-        diesel::insert(&crate_ids
-            .into_iter()
-            .map(|crate_id| {
-                queries::ExperimentCrate {
-                    experiment_id,
-                    crate_id,
-                }
-            })
-            .collect::<Vec<_>>()).into(experiment_crates::table)
-            .execute(&*conn)?;
-        let tcs = tcs.into_iter()
-            .map(|tc| {
-                queries::Toolchain { description: serde_json::to_value(tc).unwrap() }
-            })
-            .collect::<Vec<_>>();
-        diesel::insert(&tcs.on_conflict_do_nothing())
-            .into(toolchains::table)
-            .execute(&*conn)?;
-        let toolchain_ids = toolchains::table
-            .filter(toolchains::description.eq_any(tcs.into_iter().map(
-                |tc| tc.description,
-            )))
-            .select(toolchains::id)
-            .load::<i32>(&*conn)?;
-        diesel::insert(&toolchain_ids
-            .into_iter()
-            .map(|toolchain_id| {
-                queries::ExperimentToolchain {
-                    experiment_id,
-                    toolchain_id,
-                }
-            })
-            .collect::<Vec<_>>()).into(experiment_toolchains::table)
-            .execute(&*conn)?;
-
-        Ok(())
+            Ok(())
+        })
     }
     fn delete_experiment(&self, ex_name: &str) -> Result<()> {
         use db::schema::*;
         let conn = self.conn.lock().expect("Poisoined lock");
 
-        let ex: queries::Experiment = experiments::table
-            .filter(experiments::name.eq(ex_name))
-            .get_result(&*conn)?;
+        conn.transaction(|| {
+            let ex: queries::Experiment = experiments::table
+                .filter(experiments::name.eq(ex_name))
+                .get_result(&*conn)?;
 
-        diesel::delete(experiment_toolchains::table.filter(
-            experiment_toolchains::experiment_id.eq(ex.id),
-        )).execute(&*conn)?;
+            diesel::delete(experiment_toolchains::table.filter(
+                experiment_toolchains::experiment_id.eq(ex.id),
+            )).execute(&*conn)?;
 
-        diesel::delete(experiment_crates::table.filter(
-            experiment_crates::experiment_id.eq(ex.id),
-        )).execute(&*conn)?;
+            diesel::delete(experiment_crates::table.filter(
+                experiment_crates::experiment_id.eq(ex.id),
+            )).execute(&*conn)?;
 
-        diesel::delete(experiments::table.filter(
-            experiments::experiment_id.eq(ex.id),
-        )).execute(&*conn)?;
+            diesel::delete(experiments::table.filter(experiments::id.eq(ex.id)))
+                .execute(&*conn)?;
 
-        Ok(())
+            Ok(())
+        })
     }
 }
 
